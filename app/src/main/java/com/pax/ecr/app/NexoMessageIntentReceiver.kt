8e2ab13475a45400bf078a2f09d4b1e18a64cf97
GabrielMedini <gabrielmedini@gmail.com>
2025-01-29 11:30:21 +0100

    Fixed issue with android 10 and startActivity()

--- app/src/main/java/com/pax/ecr/app/NexoMessageIntentReceiver.kt
+++ app/src/main/java/com/pax/ecr/app/NexoMessageIntentReceiver.kt
@@ -49,6 +49,7 @@ class NexoMessageIntentReceiver : BroadcastReceiver() {
     private fun moveToForeground(context: Context) {
         Intent(context, MainActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }.also { context.startActivity(it) }
     }
 
