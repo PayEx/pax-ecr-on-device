abf1f43b86bb10cd35cc97b021a175bec552c2cf
Jørn Tharaldsen <jorntharaldsen@gmail.com>
2026-01-22 13:29:12 +0100

    Bumps kotlin version from 2.1.20 to 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -15,5 +18,12 @@ subprojects {
                 "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
             ),
         )
+    }
+
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+            freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
+        }
     }
 }
